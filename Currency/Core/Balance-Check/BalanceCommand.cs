6f1d8a01b38e93791c8f6412019af763935fb7ea
ThortonEllmers <ellmersthorton@gmail.com>
2026-01-01 17:40:01 +1300

    Create BalanceCommand.cs

--- Currency/Core/Balance-Check/BalanceCommand.cs
+++ Currency/Core/Balance-Check/BalanceCommand.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) 2025 HexEchoTV (CUB)
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
-// https://github.com/ThortonEllmers/Streamerbot-Commands
+// https://github.com/HexEchoTV/Streamerbot-Commands
 //
 // DEPENDENCIES: ConfigSetup.cs
 
